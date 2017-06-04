9c525644d1095cdc694d9df888eec03370b2d0df
Chris Kennedy <chrisken@gmail.com>
2017-06-04 10:29:31 -0700

    Reverse dependency checking (buggy) for CI, if SL_CRAN=true

---
+++ tests/testthat/test-revdep.R
@@ -0,0 +1,40 @@
+# This file is intended to be run prior to a release, not during
+# normal unit testing.
+
+# Based on https://github.com/HenrikBengtsson/future/tree/master/revdep
+library(SuperLearner)
+library(testthat)
+library(devtools)
+
+context("Reverse dependency checking")
+
+# Run manually or if SL_CRAN environmental variable is set and we're not in a
+# pull request build.
+# This would be done as prep for a CRAN release.
+# This is not currently working on our CI systems: Travis and Appveyor.
+if (Sys.getenv("SL_CRAN") == "true" &&
+    Sys.getenv("TRAVIS_PULL_REQUEST") %in% c("", "false") &&
+    Sys.getenv("APPVEYOR_PULL_REQUEST_NUMBER") == "") {
+
+  cat("Checking reverse dependencies.\n")
+
+  if (!requireNamespace("BiocInstaller", quietly = T)) {
+    # Manually re-install bioc, unclear why this is necessary on Appveyor.
+    source('https://bioconductor.org/biocLite.R')
+    # This will install BiocInstaller.
+    biocLite()
+  }
+
+  print(sessionInfo())
+
+  devtools::revdep_check(bioconductor = T, recursive = T,
+             threads = RhpcBLASctl::get_num_cores())
+
+  # Save results to the revdep main directory.
+  devtools::revdep_check_save_summary()
+
+  # Print any problems.
+  print(devtools::revdep_check_print_problems())
+} else {
+  cat("Skipping revdep.\n")
+}
